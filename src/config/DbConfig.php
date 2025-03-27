0df33e79dc403d1b4158569654bcc31184059abd
--replace-all <wfarat@gmail.com>
2025-03-27 03:05:17 +0100

    Use getenv as a fallback for environment variables
    Switched to using `getenv` to provide a fallback in case `$_ENV` variables are not set. This improves compatibility across different server setups where `$_ENV` may not be populated reliably.

--- src/config/DbConfig.php
+++ src/config/DbConfig.php
@@ -24,11 +24,11 @@ class DbConfig {
         $dotenv->load();
 
         // Get DB config from .env
-        $host = $_ENV['DB_HOST'];
-        $this->user = $_ENV['DB_USER'];
-        $this->pass = $_ENV['DB_PASS'];
-        $dbname = $_ENV['DB_NAME'];
-        $dbport = $_ENV['DB_PORT'];
+        $host = getenv('DB_HOST') ?: $_ENV['DB_HOST'];
+        $this->user = getenv('DB_HOST') ?: $_ENV['DB_USER'];
+        $this->pass = getenv('DB_HOST') ?: $_ENV['DB_PASS'];
+        $dbname = getenv('DB_HOST') ?: $_ENV['DB_NAME'];
+        $dbport = getenv('DB_HOST') ?: ['DB_PORT'];
         $this->dsn = 'mysql:host=' . $host . ';dbname=' . $dbname . ';port=' . $dbport . ';ssl-mode=require';
     }
 
